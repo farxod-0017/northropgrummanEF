0ca27ff6d18cdfb182d454806e149eebd10509e9
farxod_0017 <farxodegamnazarov2@gmail.com>
2024-10-28 20:15:57 +0500

    the end gif

--- src/pages/PageA6.jsx
+++ src/pages/PageA6.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import subs from '../comonents/subsNG.png'
 
 function Page6() {
   return (
@@ -13,9 +12,9 @@ function Page6() {
             <h2>Kompaniya ba'zi bo‘limlarda moslashuvchan ish soatlarini joriy etgan. Bu xodimlarga o‘z jadvalini moslashtirish imkonini beradi, masalan, 4/10 jadvali, bu degani, xodimlar haftaning to‘rt kunida 10 soat ishlaydi va keyin uch kun dam oladi. </h2>
           </div>
           <div className="right_img">
-            <img src="https://th.bing.com/th/id/OIP.b98LnBF4ql3R1V1gEoqu7AHaD3?rs=1&pid=ImgDetMain" alt="" />
-            <img src="https://th.bing.com/th/id/R.025f44802350adf48cb7275f2bc2cfa9?rik=8tD5gUuKDZ%2bNjA&pid=ImgRaw&r=0" alt="" />
-            <img src={subs} alt="" />
+            <img src="https://teamsurfboard.com/wp-content/uploads/2023/08/Group-777-2-1024x276.png" alt="" />
+            <img src="https://th.bing.com/th/id/R.99e9221f0ae30768661304b2c8542c2f?rik=9AgvB4JIV34%2fRw&pid=ImgRaw&r=0" alt="" />
+            <img src="https://timesles.com/calendars-pdf/uzbekistan-224/years/calendar-yearly-2023-L-uz-uzbekistan-224.jpg" alt="" />
           </div>
         </div>
       </section>
--- src/pages/PageA7.jsx
+++ src/pages/PageA7.jsx
@@ -4,7 +4,7 @@ function Page7() {
   return (
     <div>
         <section className='page7'>
-            <img style={{width:'100%', height:'100vh'}} src="https://th.bing.com/th/id/R.306d4124cb89eee44a5a075ca34e928f?rik=4KyCAlxNgH6erw&pid=ImgRaw&r=0" alt="" />
+            <img style={{width:'100%', height:'100vh'}} src="https://th.bing.com/th/id/R.2be42f5b3d3a30c8c01d040fb0817685?rik=DEily%2fu11921%2fA&pid=ImgRaw&r=0" alt="" />
         </section>
     </div>
   )
